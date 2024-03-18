64df1d0f73f40c19eb93f69312021ae3b4f04dcf
arthur <arthur.broudoux@gmail.com>
2024-03-18 09:26:05 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { Banner } from 'flowbite-svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Button>Test</Button>
+
+	<Counter />
+
+	<Banner id="bottom-banner" position="absolute" bannerType="bottom">
+		<p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+			test banner
+		</p>
+	  </Banner>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
